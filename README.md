56ab4c0f729955b255a0f23d03b18d221522109e
rheinsullivan <rheinsullivan06@gmail.com>
2024-09-04 02:11:51 +0700

    update index.html, footer.jsx, package-lock.json, readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rhein Sullivan
+Author & Copyright # Rhein Sullivan
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/landingpages/Footer.jsx
+++ src/landingpages/Footer.jsx
@@ -6,38 +6,52 @@ import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <footer class="flex items-center justify-center bg-white pb-12 pt-24 dark:bg-darkcustom">
-      <div class="container mx-12 lg:mx-0">
-        <div class="flex flex-wrap">
-          <div class="mb-12 w-full px-4 font-medium text-gray-600 dark:text-gray-400 md:w-1/3">
-            <h2 class="mb-5 text-2xl font-bold uppercase text-black dark:text-white lg:text-4xl">
+    <footer className="flex items-center justify-center bg-white pb-12 pt-24 dark:bg-darkcustom">
+      <div className="container mx-12 lg:mx-0">
+        <div className="flex flex-wrap">
+          <div className="mb-12 w-full px-4 font-medium text-gray-600 dark:text-gray-400 md:w-1/3">
+            <h2 className="mb-5 text-2xl font-bold uppercase text-black dark:text-white lg:text-4xl">
               Desa Karangwuni
             </h2>
-            <h3 class="mb-2 text-lg font-bold lg:text-2xl">Hubungi Kami</h3>
+            <h3 className="mb-2 text-lg font-bold text-green dark:text-sky-600 lg:text-2xl">
+              Hubungi Kami
+            </h3>
             <div className="text-sm lg:text-base">
-              <a href="mailto:#?subject=Subject%20of%20the%20email&body=Fill%20the%20message%20here">desa.karangwuni@contoh.com</a>
+              <a href="mailto:#?subject=Subject%20of%20the%20email&body=Fill%20the%20message%20here">
+                desa.karangwuni@contoh.com
+              </a>
               <p>+6289666145666</p>
-              <p>4HGP+V4Q, Karangwuni, Kec. Sedong</p>
-              <p>Kab. Cirebon, Jawa Barat 45189</p>
+              <a href="https://maps.app.goo.gl/51oQX9uMwvnpnzEk8">
+                <p>4HGP+V4Q, Karangwuni, Kec. Sedong</p>
+                <p>Kab. Cirebon, Jawa Barat 45189</p>
+              </a>
             </div>
           </div>
-          <div class="mb-10 mt-16 w-full pl-4 md:w-1/3 lg:pl-52">
-            <h3 class="mb-5 font-semibold text-black dark:text-white lg:text-2xl">
+          <div className="mb-10 mt-[60px] w-full pl-4 md:w-1/3 lg:pl-52">
+            <h3 className="mb-3 font-semibold text-black dark:text-white lg:text-2xl">
               Support By :
             </h3>
-            <ul class="text-gray-600 dark:text-gray-400">
+            <ul className="text-gray-600 dark:text-gray-400">
               <li>
                 <a
                   href="https://rheinsullivan.my.id/"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="inline-block text-sm italic hover:text-red lg:text-base"
                 >
-                  Rhein Sullivan
+                  Moh. Rifki Ramadhan
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
+                >
+                  HM. Djajuli, MM
                 </a>
               </li>
               <li>
                 <a
                   href="https://kelompok37karangwuni.vercel.app/"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Kelompok KKM : 37
                 </a>
@@ -45,25 +59,25 @@ const Footer = () => {
               <li>
                 <a
                   href="https://umc.ac.id/"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   UMC 2024
                 </a>
               </li>
             </ul>
           </div>
-          <div class="mb-10 mt-14 w-full pl-3 md:w-1/3 lg:pl-72">
-            <h3 class="mb-5 font-semibold text-black dark:text-white lg:text-2xl">
+          <div className="mb-10 mt-14 w-full pl-3 md:w-1/3 lg:pl-72">
+            <h3 className="mb-3 font-semibold text-black dark:text-white lg:text-2xl">
               Tautan :
             </h3>
-            <ul class="text-gray-600 dark:text-gray-400">
+            <ul className="text-gray-600 dark:text-gray-400">
               <li>
                 <Link
                   smooth={true}
                   spy={true}
                   offset={-150}
                   to="home"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Beranda
                 </Link>
@@ -74,7 +88,7 @@ const Footer = () => {
                   spy={true}
                   offset={-60}
                   to="information"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Informasi
                 </Link>
@@ -85,7 +99,7 @@ const Footer = () => {
                   spy={true}
                   offset={-60}
                   to="about"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Profil
                 </Link>
@@ -96,7 +110,7 @@ const Footer = () => {
                   spy={true}
                   offset={-60}
                   to="travel"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Wisata
                 </Link>
@@ -107,7 +121,7 @@ const Footer = () => {
                   spy={true}
                   offset={-60}
                   to="culture"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Budaya
                 </Link>
@@ -118,7 +132,7 @@ const Footer = () => {
                   spy={true}
                   offset={-60}
                   to="gallery"
-                  class="hover:text-primary mb-3 inline-block text-sm lg:text-base"
+                  className="hover:text-primary inline-block text-sm lg:text-base"
                 >
                   Galeri
                 </Link>
@@ -126,12 +140,12 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div class="w-full border-t border-gray-600 pt-5">
-          <div class="mb-5 flex items-center justify-center lg:items-end lg:justify-end">
+        <div className="w-full border-t border-gray-600 pt-5">
+          <div className="mb-5 flex items-center justify-center lg:items-end lg:justify-end">
             <a
               href="#"
               target="_blank"
-              class="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+              className="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
             >
               <FaFacebookF /> <title>Facebook</title>
             </a>
@@ -139,7 +153,7 @@ const Footer = () => {
             <a
               href="#"
               target="_blank"
-              class="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+              className="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
             >
               <BiLogoInstagramAlt />
               <title>Instagram</title>
@@ -148,7 +162,7 @@ const Footer = () => {
             <a
               href="#"
               target="_blank"
-              class="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+              className="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
             >
               <FaTiktok />
               <title>TikTok</title>
@@ -156,7 +170,7 @@ const Footer = () => {
             <a
               href="#"
               target="_blank"
-              class="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+              className="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
             >
               <FaXTwitter />
               <title>Twitter</title>
@@ -164,29 +178,27 @@ const Footer = () => {
             <a
               href="#"
               target="_blank"
-              class="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+              className="mr-3 flex h-9 w-9 items-center justify-center rounded-full text-lg text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
             >
               <title>LinkedIn</title>
               <FaYoutube />
             </a>
           </div>
-          <p class="text-center text-sm font-medium text-gray-600 dark:text-gray-400">
-            Copyright&#169; 2024{" "}
+          <p className="text-center text-sm font-medium text-gray-600 dark:text-gray-400">
+            Copyright &#169; 2024{" "}
             <a
               href="https://rheinsullivan.my.id/"
               target="_blank"
-              class="text-primary font-bold"
+              className="text-primary font-bold"
             >
-              <span className="font-bold italic text-red">
-                Rhein Sullivan.{" "}
-              </span>
+              <span className="font-bold italic text-red">Rhein Sullivan </span>
               All right recevied.
             </a>{" "}
             Mahasiswa
             <a
               href="https://umc.ac.id/"
               target="_blank"
-              class="font-bold text-red"
+              className="font-bold text-red"
             >
               {" "}
               Universitas Muhammadiyah Cirebon
